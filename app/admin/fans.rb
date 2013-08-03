a354497ecc3b6025e5e2f09f42bfe4cf04046598
Trevor Kimenye <kimenye@gmail.com>
2013-08-03 13:23:41 +0300

    fixed admin path for fans

--- app/admin/fans.rb
+++ app/admin/fans.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Fan do
-  actions :index
+  #actions :index
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Gotv::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  resources :fans
   root :to => "home#index"
   match 'subscribe' => 'home#create', :as => :subscribe
+  resources :fan
   ActiveAdmin.routes(self)
 end
