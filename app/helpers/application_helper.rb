545076fe0688ef0ff72b5399112fab8c0ef0a6f9
Trevor Kimenye <kimenye@gmail.com>
2013-08-03 10:31:30 +0300

    initial commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def is_facebook_fan(params)
+    unless params['signed_request'].nil? #are we viewing on facebook or not?
+      p = params['signed_request'].split('.')[1]
+      json = Base64.decode64(p + "=" * (4 - p.size % 4))
+      data = ActiveSupport::JSON.decode(json)
+      liked = data["page"]["liked"]
+    end
+  end
 end
