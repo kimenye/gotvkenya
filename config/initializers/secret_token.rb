9cc173f85259dc0862e31e27ee6ed394690a0f49
Trevor Kimenye <kimenye@gmail.com>
2013-08-03 09:20:06 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gotv::Application.config.secret_token = '56ea30661023d0f8123591866de4ee3f7d512d35f8b391fd7649355a43c0dec1a5266f2e8302e773b0aaf4b09587ae1090dd0f88af9f4992e3530e47c70dffb8'
