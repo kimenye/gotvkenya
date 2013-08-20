a6b5e5ad1f8d004a0e960663388c532fd0729963
Trevor Kimenye <kimenye@gmail.com>
2013-08-20 06:23:17 +0300

    added new home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Gotv::Application.routes.draw do
   root :to => "home#index"
   match 'subscribe' => 'home#create', :as => :subscribe
   match 'game' => 'home#game', :as => :game
-  
+  match 'play' => 'home#play', :as => :play
+
   resources :fan
   ActiveAdmin.routes(self)
 end
